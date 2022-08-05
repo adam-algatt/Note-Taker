52bd2631d94a011c95739cb6dc712b417021b230
adam-algatt <adam.algatt@gmail.com>
2022-08-05 13:11:23 -0700

    for heroku

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3001;
-const apiRoutes = require('./routes/api-routes/index');
-const htmlRoutes = require('./routes/html-routes/index.js');
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
